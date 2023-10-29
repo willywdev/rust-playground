68140ac00f8ef1ac4bf04eff80f9f50f1330e834
willywdev <werwein.dev@gmail.com>
2023-10-29 21:48:31 +0100

    change folder structure

---
+++ The Book/1_Hello_World/main.rs
@@ -0,0 +1,9 @@
+// fn -> function
+// main -> entry point
+// println! -> macro
+// (!) -> indicates that it's a macro and not a function  
+// (macro is a metaprogramming technique that allows the programmer to generate code at compile time)
+
+fn main() {
+    println!("Hello World!");
+}
