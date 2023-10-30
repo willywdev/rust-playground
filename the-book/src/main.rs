a6626acdd97e1d6bf5fd7f3acc100ec07eca8a1b
willywdev <werwein.dev@gmail.com>
2023-10-30 12:29:39 +0100

    add guessing game. finish learning comments next

--- the-book/src/main.rs
+++ the-book/src/main.rs
@@ -1,8 +1,12 @@
-// fn -> function
-// main -> entry point
+use std::io;
+// use = bring a library into scope
+// std = standard library
+// io = input/output library
+// :: = namespace operator (it's like a folder structure)
 
 fn main() {
-    cargo_build()
+    println!("Please pick one of the functions to get more information!");
+    guessing_game();
 }
 // Rust is an ahead-of-time compiled language,
 // meaning you can compile a program and give the executable to someone else,
@@ -28,11 +32,37 @@ fn main() {
 } */
 
 // -----------------------------------------------------------------------------
-fn cargo_build() {
+/* fn cargo_build() {
     println!("To build a Cargo project you can use:");
     println!("cargo build");
     println!("If anything goes wrong, the Compiler is gonna tell you.");
     println!("For example: Functions that are not used.");
     println!("Cargo then creates an executable in /target/debug/.");
     println!("You can run the executable with: ./<project_name>");
+} */
+
+/* fn cargo_information() {
+    println!("Cargo assumes all your code lives in the src directory in the root of your project.");
+    println!("Cargo assumes that your root directory is for configuration files, READMEs, and anything else not related to your code.");
+    println!(
+        "With cargo check you can check if your code compiles without creating an executable."
+    );
+} */
+
+fn guessing_game() {
+    println!("Guess the number:");
+    let mut _guess = String::new();
+    // let = create a variable
+    // mut = mutable (variable can be changed), default: immutable (variables cannot be changed)
+    // String::new() = create a new instance of a String
+    // :: = associated function (static method) Like object.func() in JS
+    // We need to a new instance because Strings are a complex data structure,
+    // and so we need to allocate memory on the heap. Without allocating memory,
+    // the compiler wouldn’t know how much space to reserve for the string and we
+    // couldn’t store our data in it.
+    io::stdin()
+        .read_line(&mut _guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed {_guess}");
 }
